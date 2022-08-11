0f2a4795b23ee92a5b9abf9a323a5323f7b2c201
sumioK <tap.miwo@gmail.com>
2022-08-11 09:57:49 +0900

    create pecipe model

---
+++ app/models/reipe.rb
@@ -0,0 +1,2 @@
+class Reipe < ApplicationRecord
+end
---
+++ db/migrate/20220811005147_create_reipes.rb
@@ -0,0 +1,12 @@
+class CreateReipes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reipes do |t|
+      t.integer :user_id
+      t.string :title
+      t.text :body
+      t.string :image_id
+
+      t.timestamps
+    end
+  end
+end
