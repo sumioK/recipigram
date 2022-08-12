1ba187f1bee647e4cce8c3b9551554512bce10ed
sumioK <tap.miwo@gmail.com>
2022-08-12 13:05:28 +0900

    add reipe create

--- app/controllers/reipes_controller.rb
+++ app/controllers/reipes_controller.rb
@@ -6,10 +6,22 @@ class ReipesController < ApplicationController
   end
 
   def new
-    @recipe = Reipe.new
+    @reipe = Reipe.new
 
+  end
+  def create
+    @reipe = Reipe.new(reipe_params)
+    @reipe.user/id = current_user.id
+    @reipe.save
+    redirect_to reipe_path(@reipe)
   end
 
   def edit
+  end
+
+
+  private
+  def reipe_params
+    params.require(reipe).permit(:title, :body, :image)
   end
 end
